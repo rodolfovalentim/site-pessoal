62912c62f5f8ae2466ceef2c57e53137fb547604
Rodolfo Valentim <rodolfovieiravalentim@gmail.com>
2019-04-17 11:44:31 -0300

    update site

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ hugo -t hugo-coder
 rm -rf ../rodolfovalentim.github.io
 cp public/* ../rodolfovalentim.github.io
 
-cd ~/Documents/rodolfovalentim.github.io
+cd ~/Documentos/rodolfovalentim.github.io
 git add .
 git commit -m "update site"
 git push
